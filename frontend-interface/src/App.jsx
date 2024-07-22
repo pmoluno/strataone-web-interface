cc825230938672db71e9d036d715de7bec4752c0
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-07-22 17:46:03 +0100

    initial pages

--- frontend-interface/src/App.jsx
+++ frontend-interface/src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/common/navigation/Navigation";
 function App() {
-
   return (
     <>
+      <Navbar />
       <h1>Under Construction</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
