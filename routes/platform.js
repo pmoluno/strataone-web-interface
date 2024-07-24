61cd8cba5176a21f17ba9489de1f09b115e4a58c
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-07-24 19:13:29 +0100

    initial platform route

---
+++ routes/platform.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const Admin = require("../models/admin");
+const User = require("../models/user");
+const Post = require('../models/post');
+const path = require("path");
+
+
+
+module.exports = router;
