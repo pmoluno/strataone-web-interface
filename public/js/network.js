a11a39446b9b3b47a31b80e95b8f7281a6532d14
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-07-30 04:21:00 +0100

    network page updated

---
+++ public/js/network.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//   const pricePerUnit = 0.234432; // Price per unit of HMG
+//   let previousInput = 0; // Variable to store the previous input value
+  
+//   $(".inputField").on("input", function(){
+//       const inputValue = $(this).val();
+      
+//       console.log("the val: " + inputValue);
+//       // Check if the input value has changed and is a valid number
+//       if (!isNaN(inputValue) && inputValue !== previousInput) {
+//           const totalPrice = inputValue * pricePerUnit; // Calculate the total price
+//           $("#totalPrice").text("$" + totalPrice.toFixed(2)); // Display the total price
+//           previousInput = inputValue; // Update the previous input value
+//       }
+//   });
+// });
+
+// console.log(prices);
