6362f549cd43c3a5c7bbc267b9df49300e76a9e2
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2024-07-30 08:26:48 +0100

    added method type

--- utils/priceUtils.js
+++ utils/priceUtils.js
@@ -3,7 +3,7 @@ const { networksP, networks } = require('../config/data');
 // Function to fetch price for a given coin
 async function getCoinPrice(coinTicker) {
     const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coinTicker}&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=true&include_last_updated_at=false&precision=5&x_cg_demo_api_key=CG-LqFnJJcWtaLjjgyNX1fDgqjm`;
-   
+    const options = { method: 'GET' };
     try {
       const response = await fetch(url, options);
       const json = await response.json();
